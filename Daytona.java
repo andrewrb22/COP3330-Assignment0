d61e195880833dd240d46c544db12300466d1fa0
Andrew Rodriguez <andrewrb22@gmail.com>
2022-08-29 20:03:18 -0400

    assignment set up,  menu and variables

---
+++ Daytona.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/**
+ * Andrew Rodriguez
+ * Assignment 0 - Daytona
+ * Aug 29, 2022
+ * COP3330
+ */
+
+public class Daytona {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+       
+        //Variables 
+        int choice = 0;
+        int time = 24;
+        int laps = 313;
+
+        while ( choice != 4 ) {
+            // Menu
+        System.out.println("\nWelcome to Daytona Beach!");
+        System.out.println("What would you like to do?");
+        System.out.println("1. Drive on the Beach");
+        System.out.println("2. Play Disc Golf");
+        System.out.println("3. See the race.");
+        System.out.println("4. Exit\n");
+        
+            
+        }
+
+        
+    
+    }
+    
+}
